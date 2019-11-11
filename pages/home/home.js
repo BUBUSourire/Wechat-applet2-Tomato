59e37dbf78933565833ae5891069d0ebe98d722e
Sourire <953810390@qq.com>
2019-11-11 13:42:16 +0800

    todolist初步完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,13 +4,68 @@ const app = getApp()
 
 Page({
     data: {
-        lists: [],
-        visible: true
+        lists: [{
+                id: 0,
+                text: "111111",
+                finished: true
+            },
+            {
+                id: 1,
+                text: "222222",
+                finished: true
+            },
+            {
+                id: 2,
+                text: "333333",
+                finished: false
+            },
+            {
+                id: 3,
+                text: "444444",
+                finished: true
+            },
+            {
+                id: 4,
+                text: "555555",
+                finished: false
+            },
+
+        ],
+        visible: false
     },
     confirm(e) {
         console.log(e.detail)
+        let content = e.detail
+
+        if (content) {
+            let todo = [{
+                id: this.data.lists.length + 1,
+                text: content,
+                finished: false
+            }]
+            this.data.lists = todo.concat(this.data.lists)
+            this.setData({
+                lists: this.data.lists
+            })
+            this.hideConfirm()
+        }
     },
-    cancel(e) {
-        console.log(e.detail)
+    deleteTodo(e){
+        console.log(e)
+        let index = e.currentTarget.dataset.index
+        this.data.lists[index].finished = true
+        this.setData({
+            lists:this.data.lists
+        })
+    },
+   hideConfirm() {
+        this.setData({
+            visible: false
+        })
+    },
+    showConfirm() {
+        this.setData({
+            visible: true
+        })
     }
 })
