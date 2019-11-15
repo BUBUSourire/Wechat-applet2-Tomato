1c8631f6beb43c4c5b94427f771d02c94f711aea
Sourire <953810390@qq.com>
2019-11-15 14:37:56 +0800

    完善我的页面

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,62 +5,39 @@ Page({
      * 页面的初始数据
      */
     data: {
+        tab: "history",
+        lists: {
+            "本周四": [{
+                    time: "14:00",
+                    text: "技术的女生家啊啊是就算是那架势看俺1",
+                    id: 1
+                },
+                {
+                    time: "14:00",
+                    text: "技术的女生家啊啊是就算是那架势看俺2",
+                    id: 2
+                },
+            ],
+            "本周五": [{
+                time: "14:00",
+                text: "技术的女生家啊啊是就算是那架势看俺3",
+                id: 3
+            }],
+            "本周六": [{
+                time: "14:00",
+                text: "技术的女生家啊啊是就算是那架势看俺4",
+                id: 4
+            }]
+        }
+    },
+    onShow: function() {
 
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    changeTab(e) {
+        console.log(e)
+        let name = e.currentTarget.dataset.name
+        this.setData({
+            tab:name
+        })
     }
 })
